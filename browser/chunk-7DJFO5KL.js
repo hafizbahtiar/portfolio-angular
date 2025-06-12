af303b8b6f0201ea3eedff057bf83d47852c1d72
hafizbahtiar <hafizbahtiar98@gmail.com>
2025-06-12 21:10:22 +0800

    Auto-generated commit

---
+++ browser/chunk-7DJFO5KL.js
@@ -0,0 +1,1 @@
+import{d as o}from"./chunk-VPQNJIMN.js";import"./chunk-Y4JRAT5G.js";var m=[{path:"",component:o}];export{m as HOME_ROUTES};
---
+++ browser/chunk-MKU5WSCA.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunk-QUCCMTGV.js";import"./chunk-Y4JRAT5G.js";var m=[{path:"",component:o}];export{m as DASHBOARD_ROUTES};
